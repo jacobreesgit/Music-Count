97efda189dc3e735e3bf839773bea9f61215b025
jacobreesgit <jacobrees@me.com>
2025-10-24 11:02:03 +0100

    Initial Commit

---
+++ Music Count/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music Count
+//
+//  Created by Jacob Rees on 24/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music Count/Music_CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_CountApp.swift
+//  Music Count
+//
+//  Created by Jacob Rees on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Music_CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
